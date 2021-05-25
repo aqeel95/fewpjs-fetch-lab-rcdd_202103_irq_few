a02b16900eca52b98203730912e2b09c34a7e7b9
Aqeel Aloraibi <aqeel.aloraibi@gmail.com>
2021-05-25 14:10:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-
- 
- return fetch("https://anapioficeandfire.com/api/books").then(resp => resp.json())
-  .then(json => console.log(json));
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 }
-
-
 
 function renderBooks(books) {
   const main = document.querySelector('main')
